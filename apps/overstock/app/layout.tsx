15f3626bf44f64a4f5d0b234c57f379403eb567b
Dan Laugharn <laugharn@gmail.com>
2024-08-05 15:58:11 -0500

    💅 Adds text color

--- apps/overstock/app/layout.tsx
+++ apps/overstock/app/layout.tsx
@@ -34,7 +34,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className="bg-white">
+      <body className="bg-white text-black">
         <LivePreviewInitComponent />
         <CartProvider cartPromise={cart}>
           <Navbar />
