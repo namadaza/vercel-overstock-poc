cd8a4c0751d9e5a17fb04735f02ed0c1dc28e383
Christian Hubbard <chrishubbard92@hotmail.com>
2024-08-07 10:28:01 -0700

    Removing testData, updating CategoryType

--- apps/overstock/components/category.tsx
+++ apps/overstock/components/category.tsx
@@ -1,14 +1,46 @@
+import Image from 'next/image';
 import Link from "next/link";
 import ArrowRightCircleIcon from "./icons/arrow-right-circle";
 
+type Dimension = {
+    height: number;
+    width: number;
+  };
+  
+  type PublishDetails = {
+    time: string;
+    user: string;
+    environment: string;
+    locale: string;
+  };
 
+  type ACL = {
+    [key: string]: unknown;
+  };
+  
+  type ImageType = {
+    uid: string;
+    _version: number;
+    title: string;
+    created_by: string;
+    updated_by: string;
+    created_at: string;
+    updated_at: string;
+    content_type: string;
+    file_size: string;
+    filename: string;
+    dimension: Dimension;
+    ACL: ACL;
+    parent_uid: string | null;
+    is_dir: boolean;
+    tags: string[];
+    publish_details: PublishDetails;
+    url: string;
+  };
+
+  
 export type CategoryType = {
-    image: {
-        src: string;
-        srcset: string;
-        sizes: string;
-        alt: string;
-    };
+    image: ImageType;
     title: string;
     url: string;
     _metadata?: {
@@ -16,15 +48,26 @@ export type CategoryType = {
     }
   }
 
-const Category = ({ category, beefy = false }: { category: CategoryType, beefy?: boolean }) => {
+const Category = ({ category }: { category: CategoryType }) => {
 
   return (
     <Link className="flex flex-col items-center text-center justify-start h-full" href={`https://www.overstock.com${category.url}`}>
-        <div className={`${beefy ? 'w-77 h-77' : 'w-44 h-44'} rounded-full overflow-hidden`}>
-            <img className="w-full h-full object-cover" src={category.image?.src} />
+        <div className={`rounded-full overflow-hidden`}>
+            <Image 
+                alt={category.title} 
+                className="w-full h-full object-cover" 
+                height={category.image.dimension.height} 
+                width={category.image.dimension.width} 
+                src={category.image?.url} 
+            />
         </div>
-        <div className="mt-4 pb-2 font-bold">{category.title}</div>
-        <div className="flex flex-row"><span className="mr-2">Shop now</span><ArrowRightCircleIcon  /></div>
+        <div className="mt-4 pb-2 font-bold">
+            {category.title}
+        </div>
+        <div className="flex flex-row">
+            <span className="mr-2">Shop now</span>
+            <ArrowRightCircleIcon  />
+        </div>
     </Link>
   );
 };
