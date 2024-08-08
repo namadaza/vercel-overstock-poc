43920e3f744a6fc23c887b7186248a1f6250136d
Dan Laugharn <laugharn@gmail.com>
2024-08-08 13:12:59 -0500

    ♻️ Simplifies footer

--- apps/overstock/components/layout/footer.tsx
+++ apps/overstock/components/layout/footer.tsx
@@ -1,9 +1,4 @@
 import Link from "next/link";
-
-import FooterMenu from "components/layout/footer-menu";
-import LogoSquare from "components/logo-square";
-import { getMenu } from "lib/shopify";
-import { Suspense } from "react";
 import FooterForm from "./footer-form";
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -40,8 +35,6 @@ const footerProps = [
 
 export default async function Footer() {
   const currentYear = new Date().getFullYear();
-  const skeleton = "w-full h-6 animate-pulse rounded bg-brand-red";
-  const menu = await getMenu("next-js-frontend-footer-menu");
   const copyrightName = COMPANY_NAME || SITE_NAME || "";
 
   return (
@@ -78,32 +71,7 @@ export default async function Footer() {
         </div>
       </div>
       <footer className="bg-brand-red text-sm text-white">
-        <div className="mx-auto flex container flex-col gap-6 px-4 lg:px-6 py-12 text-sm md:flex-row md:gap-12">
-          <div>
-            <Link
-              className="flex items-center gap-2 text-black md:pt-1"
-              href="/"
-            >
-              <LogoSquare size="sm" />
-              <span className="uppercase">{SITE_NAME}</span>
-            </Link>
-          </div>
-          <Suspense
-            fallback={
-              <div className="flex h-[188px] w-[200px] flex-col gap-2">
-                <div className={skeleton} />
-                <div className={skeleton} />
-                <div className={skeleton} />
-                <div className={skeleton} />
-                <div className={skeleton} />
-                <div className={skeleton} />
-              </div>
-            }
-          >
-            <FooterMenu menu={menu} />
-          </Suspense>
-        </div>
-        <div className="border-t border-neutral-200 py-6 text-sm">
+        <div className="py-6 text-sm">
           <div className="mx-auto flex container flex-col px-4 lg:px-6">
             <p>
               &copy; {currentYear} {copyrightName}
