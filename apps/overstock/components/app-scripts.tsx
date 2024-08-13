a772b9f6d02025c470cd1cdaebfc0b7ae808fcc4
Dan Laugharn <laugharn@gmail.com>
2024-08-13 17:10:55 -0500

    ♻️ Adds more scripts

--- apps/overstock/components/app-scripts.tsx
+++ apps/overstock/components/app-scripts.tsx
@@ -3,10 +3,23 @@ import Script from "next/script";
 function AppScripts() {
   return (
     <>
+      <Script id="shopify-var">{`var Shopify = Shopify || {};
+Shopify.shop = "a37a28-2.myshopify.com";
+Shopify.locale = "en";
+Shopify.currency = {"active":"USD","rate":"1.0"};
+Shopify.country = "US";
+Shopify.theme = {"name":"backyard-liquid/main","id":136037630111,"theme_store_id":null,"role":"main"};
+Shopify.theme.handle = "null";
+Shopify.theme.style = {"id":null,"handle":null};
+Shopify.cdnHost = "checkout.overstock.com/cdn";
+Shopify.routes = Shopify.routes || {};
+Shopify.routes.root = "/";`}</Script>
+      <Script id="shopify-window">{`window.Shopify = window.Shopify || {};
+  if (!window.Shopify.featureAssets) window.Shopify.featureAssets = {};
+  window.Shopify.featureAssets['shop-js'] = {"pay-button":["modules/client.pay-button_DGiZsA9f.en.esm.js","modules/chunk.common_CLByw39u.esm.js"],"init-shop-email-lookup-coordinator":["modules/client.init-shop-email-lookup-coordinator_DwUeB8MR.en.esm.js","modules/chunk.common_CLByw39u.esm.js"],"init-customer-accounts-sign-up":["modules/client.init-customer-accounts-sign-up_CH_uiZbz.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js"],"avatar":["modules/client.avatar_BTnouDA3.en.esm.js"],"init-shop-for-new-customer-accounts":["modules/client.init-shop-for-new-customer-accounts_DDBEmItA.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js"],"init-customer-accounts":["modules/client.init-customer-accounts_BqV_9gPV.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js"],"shop-pay-checkout-sheet":["modules/client.shop-pay-checkout-sheet_sRMCspMv.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js"],"shop-pay-payment-request":["modules/client.shop-pay-payment-request_BmIfI8mM.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js","modules/chunk.shop-pay_CD5DEKoq.esm.js"],"discount-app":["modules/client.discount-app_DvX-ul7r.en.esm.js","modules/chunk.common_CLByw39u.esm.js"],"payment-terms":["modules/client.payment-terms_DGNjqxX2.en.esm.js","modules/chunk.common_CLByw39u.esm.js","modules/client.login-button_9dbgpJA9.en.esm.js"],"login-button":["modules/client.login-button_9dbgpJA9.en.esm.js","modules/chunk.common_CLByw39u.esm.js"]};`}</Script>
+  <Script id="__st">{`var __st={"a":63371051167,"offset":-21600,"reqid":"bdd61387-a09c-4f78-85b3-f4a1e62be713-1723585232","pageurl":"checkout.overstock.com/","u":"cb91053e060a","p":"home"};`}</Script>
       <Script id="app-loader">
-        {`(function() {
-  function asyncLoad() {
-    var urls = ["https://cdn.tapcart.com/webbridge-sdk/webbridge.umd.js?shop=a37a28-2.myshopify.com","https://assets.tapcart.com/__tc-ck-loader/index.js?appId=CeeweiHh1L\u0026environment=production\u0026shop=a37a28-2.myshopify.com","//cdn.shopify.com/proxy/3b78d0c73fafccdc56f608bc60a9d06abec748d32dc6f56c8c07955673068d06/r4.cloud.yellow.ai/api/galaxy/genericIntegration/shopify-sdk/x1711463078876/overstock_production?id=BcOZghlvRm/xDQ%2B5JyfxeLa8DrKFvlDs8l8ARsN2MrA=\u0026shop=a37a28-2.myshopify.com\u0026sp-cache-control=cHVibGljLCBtYXgtYWdlPTkwMA","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-product-script.min.js?product_click=true\u0026product_view=true\u0026shop_domain=a37a28-2.myshopify.com\u0026shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-web-sdk-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008\u0026content_cards=false\u0026in_browser_msgs=false\u0026sdk_url=sdk.iad-03.braze.com\u0026shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-cart-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008\u0026sdk_url=sdk.iad-03.braze.com\u0026shop=a37a28-2.myshopify.com\u0026shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-checkout-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008\u0026sdk_url=sdk.iad-03.braze.com\u0026shop=a37a28-2.myshopify.com\u0026shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-email-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008\u0026sdk_url=sdk.iad-03.braze.com\u0026shop=a37a28-2.myshopify.com\u0026shop=a37a28-2.myshopify.com"];
+        {` var urls = ["https://cdn.tapcart.com/webbridge-sdk/webbridge.umd.js?shop=a37a28-2.myshopify.com","https://assets.tapcart.com/__tc-ck-loader/index.js?appId=CeeweiHh1L&environment=production&shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-product-script.min.js?product_click=true&product_view=true&shop_domain=a37a28-2.myshopify.com&shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-web-sdk-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008&content_cards=false&in_browser_msgs=false&sdk_url=sdk.iad-03.braze.com&shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-cart-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008&sdk_url=sdk.iad-03.braze.com&shop=a37a28-2.myshopify.com&shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-checkout-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008&sdk_url=sdk.iad-03.braze.com&shop=a37a28-2.myshopify.com&shop=a37a28-2.myshopify.com","https://js.appboycdn.com/shopify-scripts/0.2.0/braze-email-script.min.js?api_key=dc9d7751-8184-4d73-92ba-bc7abcb22008&sdk_url=sdk.iad-03.braze.com&shop=a37a28-2.myshopify.com&shop=a37a28-2.myshopify.com"];
     for (var i = 0; i < urls.length; i++) {
       var s = document.createElement('script');
       s.type = 'text/javascript';
@@ -14,14 +27,7 @@ function AppScripts() {
       s.src = urls[i];
       var x = document.getElementsByTagName('script')[0];
       x.parentNode.insertBefore(s, x);
-    }
-  };
-  if(window.attachEvent) {
-    window.attachEvent('onload', asyncLoad);
-  } else {
-    window.addEventListener('load', asyncLoad, false);
-  }
-})();`}
+    }`}
       </Script>
       <Script id="init-braze">{`+function(a,p,P,b,y){a.braze={};a.brazeQueue=[];for(var s="BrazeSdkMetadata DeviceProperties Card Card.prototype.dismissCard Card.prototype.removeAllSubscriptions Card.prototype.removeSubscription Card.prototype.subscribeToClickedEvent Card.prototype.subscribeToDismissedEvent Card.fromContentCardsJson ImageOnly CaptionedImage ClassicCard ControlCard ContentCards ContentCards.prototype.getUnviewedCardCount Feed Feed.prototype.getUnreadCardCount ControlMessage InAppMessage InAppMessage.SlideFrom InAppMessage.ClickAction InAppMessage.DismissType InAppMessage.OpenTarget InAppMessage.ImageStyle InAppMessage.Orientation InAppMessage.TextAlignment InAppMessage.CropType InAppMessage.prototype.closeMessage InAppMessage.prototype.removeAllSubscriptions InAppMessage.prototype.removeSubscription InAppMessage.prototype.subscribeToClickedEvent InAppMessage.prototype.subscribeToDismissedEvent InAppMessage.fromJson FullScreenMessage ModalMessage HtmlMessage SlideUpMessage User User.Genders User.NotificationSubscriptionTypes User.prototype.addAlias User.prototype.addToCustomAttributeArray User.prototype.addToSubscriptionGroup User.prototype.getUserId User.prototype.getUserId User.prototype.incrementCustomUserAttribute User.prototype.removeFromCustomAttributeArray User.prototype.removeFromSubscriptionGroup User.prototype.setCountry User.prototype.setCustomLocationAttribute User.prototype.setCustomUserAttribute User.prototype.setDateOfBirth User.prototype.setEmail User.prototype.setEmailNotificationSubscriptionType User.prototype.setFirstName User.prototype.setGender User.prototype.setHomeCity User.prototype.setLanguage User.prototype.setLastKnownLocation User.prototype.setLastName User.prototype.setPhoneNumber User.prototype.setPushNotificationSubscriptionType InAppMessageButton InAppMessageButton.prototype.removeAllSubscriptions InAppMessageButton.prototype.removeSubscription InAppMessageButton.prototype.subscribeToClickedEvent FeatureFlag FeatureFlag.prototype.getStringProperty FeatureFlag.prototype.getNumberProperty FeatureFlag.prototype.getBooleanProperty automaticallyShowInAppMessages destroyFeed hideContentCards showContentCards showFeed showInAppMessage deferInAppMessage toggleContentCards toggleFeed changeUser destroy getDeviceId getDeviceId initialize isPushBlocked isPushPermissionGranted isPushSupported logCardClick logCardDismissal logCardImpressions logContentCardImpressions logContentCardClick logCustomEvent logFeedDisplayed logInAppMessageButtonClick logInAppMessageClick logInAppMessageHtmlClick logInAppMessageImpression logPurchase openSession requestPushPermission removeAllSubscriptions removeSubscription requestContentCardsRefresh requestFeedRefresh refreshFeatureFlags requestImmediateDataFlush enableSDK isDisabled setLogger setSdkAuthenticationSignature addSdkMetadata disableSDK subscribeToContentCardsUpdates subscribeToFeedUpdates subscribeToInAppMessage subscribeToSdkAuthenticationFailures toggleLogging unregisterPush wipeData handleBrazeAction subscribeToFeatureFlagsUpdates getAllFeatureFlags logFeatureFlagImpression".split(" "),i=0;i<s.length;i++){for(var m=s[i],k=a.braze,l=m.split("."),j=0;j<l.length-1;j++)k=k[l[j]];k[l[j]]=(new Function("return function "+m.replaceAll(".","_")+"(){window.brazeQueue.push(arguments); return true}"))()}window.braze.getDeferredInAppMessage=function(){return new window.braze.InAppMessage};window.braze.getCachedContentCards=function(){return new window.braze.ContentCards};window.braze.getCachedFeed=function(){return new window.braze.Feed};window.braze.getUser=function(){return new window.braze.User};window.braze.getFeatureFlag=function(){return new window.braze.FeatureFlag};(y=p.createElement(P)).type='text/javascript';
         y.src='https://js.appboycdn.com/web-sdk/5.1/braze.min.js';
@@ -54,8 +60,7 @@ function AppScripts() {
                 braze.logCustomEvent('email-subscribe', { groupId: 7784 } );
               }
 
-              // TODO: Solve
-              // reconcileEmail(email)
+              reconcileEmail(email)
 
               const formStatusSuccess = document.querySelector(".newsletter-form-success");
               if (formStatusSuccess) {
@@ -74,11 +79,25 @@ function AppScripts() {
           );
         }
       }, 2000);`}</Script>
+      <Script id="shopify-analytics">{`window.ShopifyAnalytics = window.ShopifyAnalytics || {};
+window.ShopifyAnalytics.meta = window.ShopifyAnalytics.meta || {};
+window.ShopifyAnalytics.meta.currency = 'USD';
+var meta = {"page":{"pageType":"home"}};
+for (var attr in meta) {
+  window.ShopifyAnalytics.meta[attr] = meta[attr];
+}`}</Script>
       <Script
         src="https://ui.powerreviews.com/stable/4.1/ui.js"
         strategy="lazyOnload"
       />
-      <Script src="https://checkout.overstock.com/cdn/shop/t/69/assets/content-cards.js?v=25858787523254501921720759668" strategy="lazyOnload" />
+      <Script
+        src="https://checkout.overstock.com/cdn/shop/t/69/assets/custom-scripts.js?v=116197405177837524791719526878"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://checkout.overstock.com/cdn/shop/t/69/assets/content-cards.js"
+        strategy="lazyOnload"
+      />
     </>
   );
 }
--- apps/overstock/components/content-cards.tsx
+++ apps/overstock/components/content-cards.tsx
@@ -4,20 +4,23 @@ import { useEffect, useState } from "react";
 
 function ContentCards() {
   const [show, setShow] = useState(false);
+  const [visible, setVisible] = useState(false)
 
   useEffect(() => {
-    setShow(true);
+    setShow(true)
+    
+    new Promise((r) => setTimeout(r, 1000)).then(() => setVisible(true))
   }, []);
 
   if (show) {
     return (
       <>
         {/* @ts-expect-error */}
-        <content-cards className="content-cards-container">
+        <content-cards className={`content-cards-container ${visible ? 'opacity-100' : 'opacity-0'}`}>
           <div className="content-cards-header">
             <span className="cch--title">Notifications</span>
-            <div className="cch--button_container">
-              <button id="refresh" className="cch-btn ">
+            <div className="cch--button_container flex gap-1">
+              <button id="refresh" className="cch-btn">
                 <svg
                   width="24"
                   height="24"
@@ -26,8 +29,8 @@ function ContentCards() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M17.2214 5.61189L15 7.83333L22 9L20.8333 2L18.2866 4.54677C16.5895 3.11436 14.3948 2.25 12 2.25C6.61509 2.25 2.25 6.61509 2.25 12C2.25 17.3849 6.61509 21.75 12 21.75C15.9996 21.75 19.4347 19.3424 20.9385 15.9002C21.1043 15.5207 20.931 15.0785 20.5514 14.9127C20.1719 14.7469 19.7297 14.9202 19.5639 15.2998C18.2903 18.2152 15.3822 20.25 12 20.25C7.44351 20.25 3.75 16.5565 3.75 12C3.75 7.44351 7.44351 3.75 12 3.75C13.9811 3.75 15.7987 4.44772 17.2214 5.61189Z"
                     fill="#5A5A5A"
                   />
@@ -43,8 +46,8 @@ function ContentCards() {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M5.53033 4.46967C5.23744 4.17678 4.76256 4.17678 4.46967 4.46967C4.17678 4.76256 4.17678 5.23744 4.46967 5.53033L10.9393 12L4.46967 18.4697C4.17678 18.7626 4.17678 19.2374 4.46967 19.5303C4.76256 19.8232 5.23744 19.8232 5.53033 19.5303L12 13.0607L18.4697 19.5303C18.7626 19.8232 19.2374 19.8232 19.5303 19.5303C19.8232 19.2374 19.8232 18.7626 19.5303 18.4697L13.0607 12L19.5303 5.53033C19.8232 5.23744 19.8232 4.76256 19.5303 4.46967C19.2374 4.17678 18.7626 4.17678 18.4697 4.46967L12 10.9393L5.53033 4.46967Z"
                     fill="#5A5A5A"
                   />
@@ -54,21 +57,6 @@ function ContentCards() {
           </div>
           <div className="content-cards-main">
             <div className="content-cards-wrapper">
-              <svg
-                className="rotate"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M17.2214 5.61189L15 7.83333L22 9L20.8333 2L18.2866 4.54677C16.5895 3.11436 14.3948 2.25 12 2.25C6.61509 2.25 2.25 6.61509 2.25 12C2.25 17.3849 6.61509 21.75 12 21.75C15.9996 21.75 19.4347 19.3424 20.9385 15.9002C21.1043 15.5207 20.931 15.0785 20.5514 14.9127C20.1719 14.7469 19.7297 14.9202 19.5639 15.2998C18.2903 18.2152 15.3822 20.25 12 20.25C7.44351 20.25 3.75 16.5565 3.75 12C3.75 7.44351 7.44351 3.75 12 3.75C13.9811 3.75 15.7987 4.44772 17.2214 5.61189Z"
-                  fill="#5A5A5A"
-                />
-              </svg>
             </div>
           </div>
           {/* @ts-expect-error */}
