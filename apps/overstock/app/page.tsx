80fd3c7b1b4add5b5b67f92ff4797e01d4810371
Dan Laugharn <laugharn@gmail.com>
2024-08-19 10:42:29 -0500

    ♻️ Refactors

--- apps/overstock/app/page.tsx
+++ apps/overstock/app/page.tsx
@@ -1,26 +1,35 @@
 import Section from "components/section";
-import type { LivePreviewQuery } from "contentstack";
 import { getHomePage } from "lib/contentstack";
+import type { Metadata } from "next";
 
-export const metadata = {
-  description:
-    "Crazy-Good Deals",
-  openGraph: {
-    type: "website",
-  },
-};
+export async function generateMetadata() {
+  const homePage = await getHomePage();
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: LivePreviewQuery | undefined;
-}) {
-  const homePage = await getHomePage()
+  const metadata: Metadata = {
+    description: homePage.seo?.meta_description,
+    robots: {
+      index: !!homePage.seo?.enable_search_indexing,
+    },
+    title: homePage.seo?.meta_title,
+  };
 
-  return <div className="grid grid-cols-1 gap-y-12 py-12">
-  {homePage.sections.map(({ section }: any, index: number) => {
+  return metadata;
+}
 
-    return <Section key={section._metadata.uid} index={index} section={section} />
-  })}
-  </div>
+export default async function HomePage() {
+  const homePage = await getHomePage();
+
+  return (
+    <div className="grid grid-cols-1 gap-y-12 py-12">
+      {homePage.sections.map(({ section }: any, index: number) => {
+        return (
+          <Section
+            key={section._metadata.uid}
+            index={index}
+            section={section}
+          />
+        );
+      })}
+    </div>
+  );
 }
