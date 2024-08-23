6cd322ea3eccf4a6764aa468398e909f4d8ebaec
Jason Wiker <Jason.wiker@vercel.com>
2024-08-22 20:32:01 -0600

    Ship temp post request

--- apps/overstock/app/api/tmp-revalidate/route.ts
+++ apps/overstock/app/api/tmp-revalidate/route.ts
@@ -18,3 +18,21 @@ export async function GET(request: NextRequest) {
   // Return the response
   return Response.json({ now: Date.now(), revalidated: true });
 }
+
+export async function POST(request: NextRequest) {
+  // Look for a tag query parameter
+  const tags = request.nextUrl.searchParams.get("tags");
+
+  // If we don't have a tag, throw an error
+  if (!tags) {
+    return new Response("Unprocessable Entity", { status: 422 });
+  }
+
+  // Send the request for revalidation
+  tags.split(",").forEach((tag) => {
+    revalidateTag(tag);
+  });
+
+  // Return the response
+  return Response.json({ now: Date.now(), revalidated: true });
+}
