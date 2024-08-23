9073d0aa73656c8f57316a8736040158b6ecc380
Jason Wiker <Jason.wiker@vercel.com>
2024-08-23 08:03:31 -0600

    add timeout for shopify bc its slow to update

---
+++ apps/overstock/app/api/tmp-revalidate-shopify/route.ts
@@ -0,0 +1,38 @@
+import { revalidateTag } from "next/cache";
+import type { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // Look for a tag query parameter
+  const tags = request.nextUrl.searchParams.get("tags");
+
+  // If we don't have a tag, throw an error
+  if (!tags) {
+    return new Response("Unprocessable Entity", { status: 422 });
+  }
+
+  // Send the request for revalidation
+  tags.split(",").forEach((tag) => {
+    revalidateTag(tag);
+  });
+
+  // Return the response
+  return Response.json({ now: Date.now(), revalidated: true });
+}
+
+export async function POST(request: NextRequest) {
+  // Look for a tag query parameter
+  const tags = request.nextUrl.searchParams.get("tags");
+
+  // If we don't have a tag, throw an error
+  if (!tags) {
+    return new Response("Unprocessable Entity", { status: 422 });
+  }
+
+  // Send the request for revalidation
+  tags.split(",").forEach((tag) => {
+    revalidateTag(tag);
+  });
+  await new Promise((r) => setTimeout(r, 5000));
+  // Return the response
+  return Response.json({ now: Date.now(), revalidated: true });
+}
